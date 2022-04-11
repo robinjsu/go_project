93f326b3a84714124cb4625eb4f1e031d57c434a
Robin Su <robisu@pdx.edu>
2022-04-10 17:54:01 -0700

    remove text code

--- project/display.go
+++ project/display.go
@@ -11,15 +11,7 @@ func Display(env gui.Env) {
 
 	loadPage := func(drw draw.Image) image.Rectangle {
 		mainPage := image.Rect(0, 0, 900, 600)
-		// text := &font.Drawer{
-		// 	Dst:  drw,
-		// 	Src:  image.Black,
-		// 	Face: face,
-		// 	Dot:  fixed.P(12, 12),
-		// }
-
 		draw.Draw(drw, mainPage, image.White, image.ZP, draw.Src)
-		// text.DrawString(content)
 
 		return mainPage
 	}
