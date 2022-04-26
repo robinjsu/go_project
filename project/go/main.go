5e211f26f2a3759bd7952437c283bee5d7e8b7f6
Robin Su <robisu@pdx.edu>
2022-04-26 12:57:06 -0700

    remove paging func

--- project/go/main.go
+++ project/go/main.go
@@ -21,13 +21,10 @@ func run() {
 	words := make(chan string)
 	define := make(chan string)
 	save := make(chan Word)
-	// next := make(chan string)
-	// prev := make(chan string)
 
 	// each component is muxed from main, occupying its own thread
 	go Display(mux.MakeEnv())
 	go Text(mux.MakeEnv(), "./alice.txt", fontFaces, words)
-	// go PagingBtns(mux.MakeEnv(), next, prev, fontFaces)
 	go Search(mux.MakeEnv(), fontFaces, words, define)
 	go Define(mux.MakeEnv(), fontFaces, define, save)
 	go WordList(mux.MakeEnv(), save, "Edith Wharton")
