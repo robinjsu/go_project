c7a5cd19303acc66747d4f35e18be9ac74206652
Robin Su <robisu@pdx.edu>
2022-05-27 11:17:27 -0700

    able to play audio for each page, some internal error with calling the TTS api

--- project/go/tts.go
+++ project/go/tts.go
@@ -20,18 +20,41 @@ import (
 	"github.com/faiface/gui/win"
 )
 
-func drawAudio(img image.Image) (func(draw.Image) image.Rectangle, image.Rectangle) {
-	imgSz := img.Bounds().Size()
+func getBtnIcons() []image.Image {
+	imgFiles := []string{"images/play.png", "images/pause.png", "images/prev.png", "images/next.png"}
+	var images []image.Image
+	for _, img := range imgFiles {
+		png, err := getPNG(img)
+		if err != nil {
+			fmt.Println(err)
+		}
+		images = append(images, png)
+	}
+	return images
+}
+
+func drawAudio(images []image.Image) (func(draw.Image) image.Rectangle, []image.Rectangle) {
+	imgSz := images[0].Bounds().Size()
 	footerSz := footer.Bounds().Size()
 	pos := footer.Min.Add(image.Pt(10, (footerSz.Y-imgSz.Y)/2))
-	pngR := image.Rectangle{pos, pos.Add(img.Bounds().Size())}
+	pngR := image.Rectangle{pos, pos.Add(imgSz)}
+	iconArea := image.Rectangle{pos, pos.Add(image.Pt(imgSz.X*4, imgSz.Y))}
+	icons := image.NewRGBA(iconArea)
+	iconsR := []image.Rectangle{}
+
+	for range images {
+		iconsR = append(iconsR, pngR)
+		pngR = pngR.Add(image.Pt(imgSz.X, 0))
+	}
 
 	drawPNG := func(drw draw.Image) image.Rectangle {
-		draw.Draw(drw, pngR, img, image.ZP, draw.Over)
-
-		return pngR.Bounds()
+		for i, img := range iconsR {
+			draw.Draw(icons, img, images[i], image.ZP, draw.Over)
+		}
+		draw.Draw(drw, icons.Bounds(), icons, icons.Bounds().Min, draw.Over)
+		return icons.Bounds()
 	}
-	return drawPNG, pngR
+	return drawPNG, iconsR
 }
 
 func getSpeech(content string, outFile string) string {
@@ -69,51 +92,50 @@ func getSpeech(content string, outFile string) string {
 	return outFile
 }
 
-func playBack(audio *os.File, start <-chan bool, pause <-chan bool, restart <-chan bool, done <-chan bool) {
+// TODO: streamer still nto working!
+func playBack(audio *os.File, start <-chan bool, pause <-chan bool, restart <-chan bool, done <-chan bool, newStream <-chan beep.StreamSeekCloser) {
 	var controller *beep.Ctrl
-
-	stream, format, err := mp3.Decode(audio)
+	var streamer beep.StreamSeekCloser
+	_, format, err := mp3.Decode(audio)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer stream.Close()
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-	controller = &beep.Ctrl{Streamer: stream, Paused: false}
+	controller = &beep.Ctrl{Streamer: nil, Paused: false}
 	for {
 		select {
+		case streamer = <-newStream:
+			controller.Streamer = streamer
 		case <-start:
 			speaker.Play(controller)
-		case <-pause:
+		case paused := <-pause:
 			speaker.Lock()
-			controller.Paused = !controller.Paused
+			controller.Paused = paused
 			speaker.Unlock()
 		case <-restart:
-			err = stream.Seek(0)
+			err := streamer.Seek(0)
 			if err != nil {
 				log.Fatal(ReadError{err})
 			}
 		case <-done:
-			speaker.Lock()
-			speaker.Clear()
-			speaker.Unlock()
-			break
+			controller.Streamer = nil
 		}
 	}
 }
 
 func TextToSpeech(env gui.Env, load chan bool, content <-chan [][]string) {
-	var audioStarted = false
-	// var filename = ""
-	audioPng, err := getPNG("images/audio_icon.png")
-	if err != nil {
-		fmt.Println(err)
-	}
-	audioBtn, pngR := drawAudio(audioPng)
+	var pages [][]string
+
+	audioStarted := false
+	paused := false
+	audioBtns := getBtnIcons()
+	audioBtn, iconsR := drawAudio(audioBtns)
+	newStreamer := make(chan beep.StreamSeekCloser)
 	start := make(chan bool)
 	restart := make(chan bool)
 	pause := make(chan bool)
 	done := make(chan bool)
-	// var pages [][]string
+
 	pg := 0
 
 	for {
@@ -122,56 +144,81 @@ func TextToSpeech(env gui.Env, load chan bool, content <-chan [][]string) {
 			if ready == true {
 				env.Draw() <- audioBtn
 			}
-		// case pages = <-content:
-		// filename = getSpeech(string(strings.Join(pages[pg], " ")), fmt.Sprintf("pg-%v.mp3", pg))
-		// audio, err := os.Open(filename)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-		// go playBack(audio, start, pause, restart, done)
-		// case p := <-page:
-		// 	switch p {
-		// 	case "prev":
-		// 		if pg > 0 {
-		// 			pg--
-		// 		}
-		// 	case "next":
-		// 		if pg < len(pages)-1 {
-		// 			pg++
-		// 		}
-		// 	}
-		// done <- true
-		// filename = getSpeech(string(strings.Join(pages[pg], " ")), fmt.Sprintf("pg-%v.mp3", pg))
-		// audio, err := os.Open(filename)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-		// go playBack(audio, start, pause, restart, done)
+		case pages = <-content:
+			fmt.Println(pages[0])
+			// getSpeech(string(strings.Join(pages[0], " ")), fmt.Sprintf("%s/pg-%v.mp3", audioDir, i))
 		case e, ok := <-env.Events():
 			if !ok {
+				// audioFiles, err := os.ReadDir(audioDir)
+				// if err != nil {
+				// 	log.Fatal(err)
+				// }
+				// for _, audio := range audioFiles {
+				// 	os.Remove(audio.Name())
+				// }
 				close(env.Draw())
 				return
 			}
 			switch e := e.(type) {
 			case win.MoDown:
-				if e.Point.In(pngR) {
-					if audioStarted {
+				switch {
+				case e.Point.In(iconsR[0]):
+					fmt.Println("play")
+					if audioStarted && paused == false {
 						restart <- true
+					} else if audioStarted && paused == true {
+						paused = false
+						pause <- false
 					} else {
-						audio, err := os.Open(fmt.Sprintf("pg-%v.mp3", pg))
+						audio, err := os.Open(fmt.Sprintf("%s/pg-%v.mp3", audioDir, pg))
 						if err != nil {
 							log.Fatal(err)
 						}
-						go playBack(audio, start, pause, restart, done)
+						audioStream, _, _ := mp3.Decode(audio)
+						go playBack(audio, start, pause, restart, done, newStreamer)
+						newStreamer <- audioStream
 						start <- true
 						audioStarted = true
+					}
+				case e.Point.In(iconsR[1]):
+					fmt.Println("pause")
+					paused = true
+					pause <- paused
+				case e.Point.In(iconsR[2]):
+					fmt.Println("prev")
+					if pg > 0 {
+						pg--
+						done <- true
+						audio, err := os.Open(fmt.Sprintf("%s/pg-%v.mp3", audioDir, pg))
+						if err != nil {
+							log.Fatal(err)
+						}
+						streamer, _, err := mp3.Decode(audio)
+						if err != nil {
+							log.Fatal(err)
+						}
+						newStreamer <- streamer
+					}
+				case e.Point.In(iconsR[3]):
+					fmt.Println("next")
+					if pg < len(pages)-1 {
+						pg++
+						done <- true
+						audio, err := os.Open(fmt.Sprintf("%s/pg-%v.mp3", audioDir, pg))
+						if err != nil {
+							log.Fatal(err)
+						}
+						streamer, _, _ := mp3.Decode(audio)
+						newStreamer <- streamer
 					}
 				}
 			case win.KbDown:
 				if e.Key == win.KeySpace {
-					pause <- true
+					paused = !paused
+					pause <- paused
 				}
 			}
 		}
+
 	}
 }
